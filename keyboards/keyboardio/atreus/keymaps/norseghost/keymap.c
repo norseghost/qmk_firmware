36277d96671577b4e69ab5b8a339bf0a9548d56a
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2021-05-08 06:24:07 +0200

    gc gcaa

--- keyboards/keyboardio/atreus/keymaps/norseghost/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/norseghost/keymap.c
@@ -1,44 +1,100 @@
-// Copyright (C) 2019, 2020  Keyboard.io, Inc
-//
-// this is the style you want to emulate.
-// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
-
-#include QMK_KEYBOARD_H
 
 #include "norseghost.h"
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   LOWER,                          RAISE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_SPC, LT(_SYMBOLS,KC_BSPC), LT(_SYMBOLS, KC_ENTER), KC_SPC,  KC_RALT, KC_MINS, KC_QUOT, KC_ENT ),
+/*
+ * The `LAYOUT_atreus_base` macro is a template to allow the use of identical
+ * modifiers for the default layouts (eg QWERTY, Colemak, Dvorak, etc), so
+ * that there is no need to set them up for each layout, and modify all of
+ * them if I want to change them.  This helps to keep consistency and ease
+ * of use. K## is a placeholder to pass through the individual keycodes
+ */
 
-  /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
-   */
-  [_SYMBOLS] = LAYOUT( /* [> RAISE <] */
-    KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
-    KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_BSLS,
-    KC_LBRC, KC_RBRC, KC_HASH, KC_LBRC, KC_RBRC, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
-   KC_TRNS, KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
-  /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
-   */
-  [_LOWER] = LAYOUT(
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QWERTY), KC_PSCR, KC_SLCK, KC_PAUS ),
-  [_RAISE] = LAYOUT(
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QWERTY), KC_PSCR, KC_SLCK, KC_PAUS )
+// clang-format off
+
+#define LAYOUT_atreus_base( \
+    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, \
+    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, \
+    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A  \
+  ) \
+  LAYOUT_wrapper( \
+     K01,         K02,         K03,         K04,           K05,                             K06,     K07,         K08,         K09,         K0A,         \
+     LCTL_T(K11), LALT_T(K12), LGUI_T(K13), LSFT_T(K14),   LT(_NAV,K15),                    K16,     RSFT_T(K17), RGUI_T(K18), LALT_T(K19), RCTL_T(K1A), \
+     K21,         K22,         K23,         K24,           K25,          LOWER,    RAISE,   K26,     K27,         K28,         K29,         K2A,         \
+     KC_ESC,      KC_TAB,      KC_LALT,     KC_LGUI,       KC_SPC,       SYM_BKSP, SYM_ENT, KC_SPC,  KC_RGUI,     KC_RALT,     KC_RCTL,     KC_TAB       \
+     )
+#define LAYOUT_atreus_base_wrapper(...)       LAYOUT_atreus_base(__VA_ARGS__)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT_atreus_base_wrapper(
+    _________________QWERTY_L1_________________, _________________QWERTY_R1_________________,
+    _________________QWERTY_L2_________________, _________________QWERTY_R2_________________,
+    _________________QWERTY_L3_________________, _________________QWERTY_R3_________________
+  ),
+
+  [_COLEMAK] = LAYOUT_atreus_base_wrapper(
+    _________________COLEMAK_L1________________, _________________COLEMAK_R1________________,
+    _________________COLEMAK_L2________________, _________________COLEMAK_R2________________,
+    _________________COLEMAK_L3________________, _________________COLEMAK_R3________________
+  ),
+
+  [_DVORAK] = LAYOUT_atreus_base_wrapper(
+    _________________DVORAK_L1_________________, _________________DVORAK_R1_________________,
+    _________________DVORAK_L2_________________, _________________DVORAK_R2_________________,
+    _________________DVORAK_L3_________________, _________________DVORAK_R3_________________
+  ),
+
+  [_WORKMAN] = LAYOUT_atreus_base_wrapper(
+    _________________WORKMAN_L1________________, _________________WORKMAN_R1________________,
+    _________________WORKMAN_L2________________, _________________WORKMAN_R2________________,
+    _________________WORKMAN_L3________________, _________________WORKMAN_R3________________
+  ),
+
+  [_NORMAN] = LAYOUT_atreus_base_wrapper(
+    _________________NORMAN_L1_________________, _________________NORMAN_L1_________________,
+    _________________NORMAN_L2_________________, _________________NORMAN_R2_________________,
+    _________________NORMAN_L3_________________, _________________NORMAN_R3_________________
+  ),
+
+  [_MALTRON] = LAYOUT_atreus_base_wrapper(
+    _________________MALTRON_L1________________, _________________MALTRON_R1________________,
+    _________________MALTRON_L2________________, _________________MALTRON_R2________________,
+    _________________MALTRON_L3________________, _________________MALTRON_R3________________
+  ),
+
+  [_EUCALYN] = LAYOUT_atreus_base_wrapper(
+    _________________EUCALYN_L1________________, _________________EUCALYN_R1________________,
+    _________________EUCALYN_L2________________, _________________EUCALYN_R2________________,
+    _________________EUCALYN_L3________________, _________________EUCALYN_R3________________
+  ),
+
+  [_CARPLAX] = LAYOUT_atreus_base_wrapper(
+    _____________CARPLAX_QFMLWY_L1_____________, _____________CARPLAX_QFMLWY_R1_____________,
+    _____________CARPLAX_QFMLWY_L2_____________, _____________CARPLAX_QFMLWY_R2_____________,
+    _____________CARPLAX_QFMLWY_L3_____________, _____________CARPLAX_QFMLWY_R3_____________
+  ),
+
+  [_SYMBOLS] = LAYOUT_atreus_base_wrapper(
+    _________________SYMBOLS_L1________________, _________________SYMBOLS_R1________________,
+    _________________SYMBOLS_L2________________, _________________SYMBOLS_R2________________,
+    _________________SYMBOLS_L3________________, _________________SYMBOLS_R3________________
+  ),
+
+  [_LOWER] = LAYOUT_atreus_base_wrapper(
+    _________________LOWER_L1__________________, _________________LOWER_R1__________________,
+    _________________LOWER_L2__________________, _________________LOWER_R2__________________,
+    _________________LOWER_L3__________________, _________________LOWER_R3__________________
+  ),
+
+  [_RAISE] = LAYOUT_atreus_base_wrapper(
+    _________________RAISE_L1__________________, _________________RAISE_R1__________________,
+    _________________RAISE_L2__________________, _________________RAISE_R2__________________,
+    _________________RAISE_L3__________________, _________________RAISE_R3__________________
+  ),
+
+  [_NAV] = LAYOUT_atreus_base_wrapper(
+    ___________________BLANK___________________, ___________________NAV_1___________________,
+    ___________________BLANK___________________, ___________________NAV_2___________________,
+    ___________________BLANK___________________, ___________________BLANK___________________
+  )
 };
